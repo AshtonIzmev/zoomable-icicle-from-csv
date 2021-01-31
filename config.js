fa587e2711b08ff5e7ba8007595840f0fbf5d22a
Issam <biossamu@gmail.com>
2021-01-31 22:34:34 +0100

    Switch to english

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const aggKey = "montant";
+const aggKey = "amount";
 const csvFile = "http://localhost:8000/data/flare.csv";
 
-const breakCategories = [["c1", "Categorie 1"], ["c2", "Categorie 2"], ["c3", "Categorie 3"], ["c4", "Categorie 4"]]
+const breakCategories = [["c1", "Category 1"], ["c2", "Category 2"], ["c3", "Category 3"], ["c4", "Category 4"]]
 
 const initialOrder = ["c1", "c2", "c3"];
-const initialLib = "Tout";
+const initialLib = "All";
 const maxDepth = Math.min(3, breakCategories.length);
